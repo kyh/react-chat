63f4e7385d5f59363328f4a85e00561f6b796da4
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-04-26 23:28:14 -0400

    Added dispatcher and message store

---
+++ public/src/js/stores/messages.js
@@ -0,0 +1,54 @@
+var Dispatcher = require('../dispatchers/dispatcher');
+var EventEmmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+
+var messages = {
+  2: {
+    user: {
+      profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
+      id: 2,
+      name: 'Ryan Clark',
+      status: 'online'
+    },
+    lastAccess: {
+      recipient: 1424469794050,
+      currentUser: 1424469794080
+    },
+    messages: [
+        {
+          contents: 'Hey!',
+          from: 2,
+          timestamp: 1424469793023
+        },
+        {
+          contents: 'Hey, what\'s up?',
+          from: 1,
+          timestamp: 1424469794000
+        }
+    ]
+  }
+};
+
+var openChatID = parseInt(Object.keys(messages)[0], 10);
+
+var messagesStore = assign({}, EventEmitter.prototype, {
+  addChangeListener: function(callback){
+    this.on('change', callback);
+  },
+  removeChangeListener: function(callback){
+    this.off('change', callback);
+  },
+  getOpenChatUserID: function(){
+    return openChatID;
+  },
+  getChatByUserID: function(id){
+    return messages[id];
+  },
+  getAll: function(){
+    return messages;
+  }
+});
+
+messagesStore.dispatchToken = Dispatcher.register(function(payload){
+
+});
