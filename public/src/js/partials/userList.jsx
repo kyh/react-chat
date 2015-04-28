aeac5954db5b11d40d981aeca987761af6af732d
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-04-27 21:27:31 -0400

    Linked View to messagesStore

--- public/src/js/partials/userList.jsx
+++ public/src/js/partials/userList.jsx
@@ -3,7 +3,7 @@ var MessagesStore = require('../stores/messages');
 var UserStore = require('../stores/user');
 
 var UserList = React.createClass({
-	getInitialState: function() {
+  getInitialState: function() {
     var allMessages = MessagesStore.getAllChats();
     var messageList = [];
 
@@ -21,74 +21,74 @@ var UserList = React.createClass({
       openChatID: MessagesStore.getOpenChatUserID(),
       messageList: messageList
     };
-	},
-	render: function () {
-		this.state.messageList.sort(function (a, b) {
-			if (a.lastMessage.timestamp > b.lastMessage.timestamp) {
-				return -1;
-			}
-			if (a.lastMessage.timestamp < b.lastMessage.timestamp) {
-				return 1;
-			}
-			return 0;
-		});
+  },
+  render: function () {
+    this.state.messageList.sort(function (a, b) {
+      if (a.lastMessage.timestamp > b.lastMessage.timestamp) {
+        return -1;
+      }
+      if (a.lastMessage.timestamp < b.lastMessage.timestamp) {
+        return 1;
+      }
+      return 0;
+    });
 
-		var messages = this.state.messageList.map(function (message, index) {
-			var date = utils.getNiceDate(message.lastMessage.timestamp);
+    var messages = this.state.messageList.map(function (message, index) {
+      var date = utils.getNiceDate(message.lastMessage.timestamp);
 
-			var statusIcon;
-			if (message.lastMessage.from !== message.user.id) {
-				statusIcon = (
-					<i className="fa fa-reply user-list__item__icon" />
-				);
-			}
-			if (message.lastAccess.currentUser < message.lastMessage.timestamp) {
-				statusIcon = (
-					<i className="fa fa-circle user-list__item__icon" />
-				);
-			}
+      var statusIcon;
+      if (message.lastMessage.from !== message.user.id) {
+        statusIcon = (
+          <i className="fa fa-reply user-list__item__icon" />
+        );
+      }
+      if (message.lastAccess.currentUser < message.lastMessage.timestamp) {
+        statusIcon = (
+          <i className="fa fa-circle user-list__item__icon" />
+        );
+      }
 
-			var isNewMessage = false;
-			if (message.lastAccess.currentUser< message.lastMessage.timestamp) {
-				isNewMessage = message.lastMessage.from !== UserStore.user.id;
-			}
+      var isNewMessage = false;
+      if (message.lastAccess.currentUser< message.lastMessage.timestamp) {
+        isNewMessage = message.lastMessage.from !== UserStore.user.id;
+      }
 
-			var itemClasses = React.addons.classSet({
-				'user-list__item': true,
-				'clear': true,
-				'user-list__item--new': isNewMessage,
-				'user-list__item--active': this.state.openChatID === message.user.id
-			});
+      var itemClasses = React.addons.classSet({
+        'user-list__item': true,
+        'clear': true,
+        'user-list__item--new': isNewMessage,
+        'user-list__item--active': this.state.openChatID === message.user.id
+      });
 
-			return (
-				<li className={ itemClasses } key={ message.user.id }>
-					<div className="user-list__item__picture">
-						<img src={ message.user.profilePicture } />
-					</div>
-					<div className="user-list__item__details">
-						<h4 className="user-list__item__name">
-							{ message.user.name }
+      return (
+        <li className={ itemClasses } key={ message.user.id }>
+          <div className="user-list__item__picture">
+            <img src={ message.user.profilePicture } />
+          </div>
+          <div className="user-list__item__details">
+            <h4 className="user-list__item__name">
+              { message.user.name }
 
-							<abbr className="user-list__item__timestamp">
-								{ date }
-							</abbr>
-						</h4>
-						<span className="user-list__item__message">
-							{ statusIcon } { message.lastMessage.contents }
-						</span>
-					</div>
-				</li>
-			)
-		}, this);
+              <abbr className="user-list__item__timestamp">
+                { date }
+              </abbr>
+            </h4>
+            <span className="user-list__item__message">
+              { statusIcon } { message.lastMessage.contents }
+            </span>
+          </div>
+        </li>
+      )
+    }, this);
 
-		return (
-			<div className="user-list">
-				<ul className="user-list__list">
-					{ messages }
-				</ul>
-			</div>
-		);
-	}
+    return (
+      <div className="user-list">
+        <ul className="user-list__list">
+          { messages }
+        </ul>
+      </div>
+    );
+  }
 });
 
 module.exports = UserList;
