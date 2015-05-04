8bd2ce472d2770960a8cd9e438d9064b5ec9ff4c
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-03 20:54:42 -0400

    Moved utils to different folder, added socket utility

---
+++ public/src/js/utils/socket.js
@@ -0,0 +1,18 @@
+var MessagesActions = require('../actions/messages');
+
+var ws = new WebSocket("ws://localhost:1337/ws");
+
+ws.onclose = function(e) {
+  console.log('socket closed');
+};
+
+ws.onopen = function(e) {
+  console.log('socket opened');
+};
+
+ws.onmessage = function(e){
+  console.log('Message from server', e.data);
+  // emit action
+};
+
+module.exports = ws;
