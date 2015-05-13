a82ff9b7223c22f51b4be2ea1f4a13c120a5e613
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-12 23:10:02 -0400

    Removed unnecessary configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@
  var node_modules_dir = path.join(__dirname, 'node_modules');
 
  var config = {
-   addVendor: function (name, path) {
-     this.resolve.alias[name] = path;
-     this.module.noParse.push(path);
-   },
    context: __dirname,
    entry: {
      app: ['webpack/hot/dev-server', './client/app/app.js']
@@ -15,9 +11,6 @@
      publicPath: '/',
      path: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? './dist/' : './build'),
      filename: 'bundle.js'
-   },
-   resolve: {
-     alias: {}
    },
    module: {
     noParse: [],
