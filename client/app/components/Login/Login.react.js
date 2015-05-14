e66e8ecd0365e8d7cb5eff33d2abaf44e75ed7e1
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-13 21:24:02 -0400

    Added content to login screen

--- client/app/components/Login/Login.react.js
+++ client/app/components/Login/Login.react.js
@@ -5,16 +5,25 @@ import { Navigation } from 'react-router';
 let Login = React.createClass({
   mixins: [Navigation],
 
-  handleLogin() {
-    console.log('Logging in');
+  handleLogin(e) {
+    e.preventDefault();
     this.transitionTo('/chat');
   },
 
   render() {
     return(
       <div className="login-page">
-        <div>Welcome to login</div>
-        <button onClick={this.handleLogin}>Login</button>
+        <form className="login-form" onSubmit={this.handleLogin}>
+          <div>
+            <span className="login-form__icon"></span>
+            <input className="login-form__input" type="text" placeholder="Name" required />
+          </div>
+          <div>
+            <span className="login-form__icon"></span>
+            <input className="login-form__input" type="email" placeholder="Email" required />
+          </div>
+          <input className="login-form__submit" type="submit" value="Sign In" />
+        </form>
       </div>
     );
   }
