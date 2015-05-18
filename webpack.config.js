3c52311fc27fe82a64d9c6fa69fa3054f8e0ab8b
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-17 23:12:38 -0400

    Added autoprefixr to scss builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
- var webpack = require('webpack');
- var path = require('path');
- var node_modules_dir = path.join(__dirname, 'node_modules');
+ const webpack = require('webpack');
+ const ExtractTextPlugin = require("extract-text-webpack-plugin");
+ const path = require('path');
 
- var config = {
+ const scssLoaders = [
+   'css-loader',
+   'autoprefixer-loader?browsers=last 2 version',
+   'sass-loader?outputStyle=compressed'
+ ];
+ 
+ const config = {
    context: __dirname,
    entry: {
      app: ['webpack/hot/dev-server', './client/app/app.js']
+   },
+   module: {
+    noParse: [],
+    loaders: [
+      { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", scssLoaders.join("!")) },
+      { test: /\.(woff|png)$/, loader: 'url-loader?limit=100000'}
+    ]
    },
    output: {
      publicPath: '/',
      path: path.resolve(__dirname, process.env.NODE_ENV === 'production' ? './dist/' : './build'),
      filename: 'bundle.js'
    },
-   module: {
-    noParse: [],
-    loaders: [
-      { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: [node_modules_dir] },
-      { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.(woff|png)$/, loader: 'url-loader?limit=100000'}
-    ]
-   },
    plugins: [
+     new ExtractTextPlugin('styles/style.css'),
      new webpack.NoErrorsPlugin(),
      new webpack.optimize.CommonsChunkPlugin('app', null, false)
    ]
