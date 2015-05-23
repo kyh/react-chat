fbdd1e1dbc7ae204439dd8d2f9543ccf7eb8bb30
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-23 12:01:01 -0400

    Added more animation utility functions, register velocity events

--- client/app/components/ReactUtils/Animation.react.js
+++ client/app/components/ReactUtils/Animation.react.js
@@ -1,11 +1,26 @@
-export function addAnimation(el, className) {
+import Velocity from 'velocity-animate';
+import 'velocity-animate/velocity.ui';
+
+export function addAnimation(options = {el: null, className: '', delay: 0, beforeBegin: null }) {
+  var {el, className, delay, beforeBegin} = options;
   return new Promise((resolve) => {
-    el.classList.add(className);
-    onAnimationEnd(el).then(() => {
-      el.classList.remove(className);
-      resolve(el);
-    });
+    setTimeout(() => {
+      if (beforeBegin) beforeBegin(el, className);
+      el.classList.add(className);
+      onAnimationEnd(el).then(() => {
+        el.classList.remove(className);
+        resolve(el);
+      });
+    }, delay);
   });
+}
+
+export function addAnimations(arr = []) {
+  arr.forEach(addAnimation);
+}
+
+export function onAnimationStart(el) {
+  return _useEventType('animationStart', el);
 }
 
 export function onAnimationEnd(el) {
@@ -22,17 +37,25 @@ function whichEvents(){
       'transition':'transitionend',
       'OTransition':'oTransitionEnd',
       'MozTransition':'transitionend',
-      'WebkitTransition':'webkitTransitionEnd'
+      'WebkitTransition':'webkitTransitionEnd',
     };
     const animations = {
       'animation':'animationend',
       'mozAnimation':'animationend',
-      'webkitAnimation':'webkitAnimationEnd'
+      'webkitAnimation':'webkitAnimationEnd',
+      'MSAnimation': 'MSAnimationEnd'
+    };
+    const animationsS = {
+      'animation':'animationstart',
+      'mozAnimation':'animationstart',
+      'webkitAnimation':'webkitAnimationStart',
+      'MSAnimation': 'MSAnimationStart'
     };
 
     return {
       transitionEnd: _getEventType(transitions, el),
-      animationEnd: _getEventType(animations, el)
+      animationEnd: _getEventType(animations, el),
+      animationStart: _getEventType(animationsS, el)
     };
 }
 
@@ -54,11 +77,23 @@ function _useEventType(type, el){
   });
 }
 
+function registerVelocityEvents() {
+  Velocity.RegisterEffect('login.bounceIn', {
+    defaultDuration: 1200,
+    calls: [
+        [ { opacity: [ 1, 0 ], scaleX: [ 1.03, 0.3 ], scaleY: [ 1.03, 0.3 ] }, 0.40 ],
+        [ { scaleX: 0.95, scaleY: 0.92, translateZ: 0 }, 0.20 ],
+        [ { scaleX: 1, scaleY: 1 }, 0.50 ]
+    ]
+  });
+}
+
 var EVENTS;
 
 const AnimationEvents = {
   init() {
     EVENTS = whichEvents();
+    registerVelocityEvents();
   }
 };
 
