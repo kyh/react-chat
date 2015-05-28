2055186193abe129a02ed276d10d1f6551494efb
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-28 19:50:57 -0400

    Setting up sockets

--- server/socket.js
+++ server/socket.js
@@ -3,17 +3,17 @@ var Config          = require('../config/base');
 
 module.exports = function() {
 
-    var wss = new WebSocketServer({
-        port: Config.socketPort
-    });
+  var wss = new WebSocketServer({
+    port: Config.socketPort
+  });
 
-    wss.on('connection', function(ws) {
-        console.log('user connected');
-        ws.on('message', function(data) {
-            var msg = JSON.parse(data);
-            console.log(msg);
-        });
+  wss.on('connection', function(ws) {
+    console.log('user connected');
+    ws.on('message', function(data) {
+      var msg = JSON.parse(data);
+      console.log(msg);
     });
+  });
 
 
 };
