2f99c89a8911ef2deba70ce8c1622d430fc602e4
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-29 07:39:35 -0400

    Removed bulk of http server, going to use Nginx to serve site

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-// modules =================================================
-var path            = require('path');
-var express         = require('express');
-var mongoose        = require('mongoose');
-var flash           = require('connect-flash');
+// Modules =================================================
+var mongoose = require('mongoose');
+var config   = require('./config/base');
+var db       = require('./config/db');
 
-var morgan          = require('morgan');
-var cookieParser    = require('cookie-parser');
-var bodyParser      = require('body-parser');
-var session         = require('express-session');
-var methodOverride  = require('method-override');
-
-var app             = express();
-var router          = express.Router();
-
-// configuration ===========================================
-var config          = require('./config/base');
-var db              = require('./config/db');
-
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
-
-// get all data/stuff of the body (POST) parameters
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/client'));
-app.use('/api', router);
-
-// routes ==================================================
-require('./server/routes')(app);
+// Websocket Server ========================================
 require('./server/socket')();
-
-exports = module.exports = app; // expose app
