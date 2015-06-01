cd5829f191a7c9a74a95f9b17a9fb3f9ac31d062
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-31 22:23:09 -0400

    Moved store to back end, added functions to receive messages

--- client/app/stores/MessagesStore.js
+++ client/app/stores/MessagesStore.js
@@ -2,80 +2,15 @@ import assign from 'object-assign';
 import ChatConstants from '../constants/ChatConstants';
 let ActionTypes = ChatConstants.ActionTypes;
 
-import UserStore from '../stores/UserStore';
+import UserStore from './UserStore';
 import Dispatcher from '../dispatchers/ChatDispatcher';
 import { EventEmitter } from 'events';
 
 // Will need to pull these messages in from server
-var messages = {
-  2: {
-    user: {
-      profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
-      id: 2,
-      name: 'Ryan Clark',
-      status: 'online'
-    },
-    lastAccess: {
-      recipient: 1424469794050,
-      currentUser: 1424469794080
-    },
-    messages: [
-      {
-        contents: 'Hey!',
-        from: 2,
-        timestamp: 1424469793023
-      },
-      {
-        contents: 'Hey, what\'s up?',
-        from: 1,
-        timestamp: 1424469794000
-      }
-    ]
-  },
-  3: {
-    user: {
-      read: true,
-      profilePicture: 'https://avatars3.githubusercontent.com/u/2955483?v=3&s=460',
-      name: 'Jilles Soeters',
-      id: 3,
-      status: 'online'
-    },
-    lastAccess: {
-      recipient: 1424352522000,
-      currentUser: 1424352522080
-    },
-    messages: [
-      {
-        contents: 'Want a game of ping pong?',
-        from: 3,
-        timestamp: 1424352522000
-      }
-    ]
-  },
-  4: {
-    user: {
-      name: 'Todd Motto',
-      id: 4,
-      profilePicture: 'https://avatars1.githubusercontent.com/u/1655968?v=3&s=460',
-      status: 'online'
-    },
-    lastAccess: {
-      recipient: 1424423579000,
-      currentUser: 1424423574000
-    },
-    messages: [
-      {
-        contents: 'Please follow me on twitter I\'ll pay you',
-        timestamp: 1424423579000,
-        from: 4
-      }
-    ]
-  }
-};
+var messages = {};
+var openChatID = null;
 
-let openChatID = parseInt(Object.keys(messages)[0], 10);
-
-let MessagesStore = assign({}, EventEmitter.prototype, {
+const MessagesStore = assign({}, EventEmitter.prototype, {
   addChangeListener(callback) {
     this.addListener('change', callback);
   },
@@ -93,7 +28,7 @@ let MessagesStore = assign({}, EventEmitter.prototype, {
   }
 });
 
-let messageStoreActions = {
+const messageStoreActions = {
   [ActionTypes.CHANGE_CHAT_WINDOW]: (payload) => {
     openChatID = payload.userID;
     messages[openChatID].lastAccess.currentUser = +new Date();
@@ -111,6 +46,13 @@ let messageStoreActions = {
     });
 
     MessagesStore.emit('change');
+  },
+  [ActionTypes.RECEIVE_MESSAGE]: (payload) => {
+
+  },
+  [ActionTypes.RECEIVE_ALL_MESSAGES]: (payload) => {
+    messages = payload.connections;
+    openChatID = parseInt(Object.keys(messages)[0], 10);
   }
 };
 
