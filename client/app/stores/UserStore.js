398b601065454dbccd864a765f60d2f54dcffa57
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-06-01 20:50:44 -0400

    Update UserStore upon logging in

--- client/app/stores/UserStore.js
+++ client/app/stores/UserStore.js
@@ -1,9 +1,36 @@
-let UserStore = {
-  user: {
-    id: 1,
-    name: 'John Doe',
-    profilePicture: 'https://avatars1.githubusercontent.com/u/8901351?v=3&s=200'
+import assign from 'object-assign';
+import ChatConstants from '../constants/ChatConstants';
+let ActionTypes = ChatConstants.ActionTypes;
+
+import Dispatcher from '../dispatchers/ChatDispatcher';
+import { EventEmitter } from 'events';
+
+var currentUser = {};
+
+const UserStore = assign({}, EventEmitter.prototype, {
+  addChangeListener(callback) {
+    this.addListener('change', callback);
+  },
+  removeChangeListener(callback) {
+    this.removeListener('change', callback);
+  },
+  getCurrentUser() {
+    return currentUser;
+  },
+  getCurrentUserID() {
+    return currentUser.id;
+  }
+});
+
+const userStoreActions = {
+  [ActionTypes.LOGIN]: (payload) => {
+    currentUser = payload.currentUser;
+    UserStore.emit('change');
   }
 };
+
+UserStore.dispatchToken = Dispatcher.register((payload) => {
+  userStoreActions[payload.type] && userStoreActions[payload.type](payload);
+});
 
 export default UserStore;
